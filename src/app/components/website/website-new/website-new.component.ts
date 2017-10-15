29627b666ae524c06a8a217546d0d950b3859188
Arnold Esguerra <ajxesg@gmail.com>
2017-10-15 16:23:13 -0400

    website new added

--- src/app/components/website/website-new/website-new.component.ts
+++ src/app/components/website/website-new/website-new.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {WebsiteService} from '../../../services/website.service.client';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-website-new',
@@ -11,15 +11,33 @@ export class WebsiteNewComponent implements OnInit {
 
   userId: string;
   websites = [{}];
-  constructor(private websiteService: WebsiteService,  private activatedRoute: ActivatedRoute) { }
+  websitename: string;
+  websitedescription: string;
+  aNewWebsite: any;
+  errorFlag: boolean;
+  errorMsg = 'Those fields cannot be blank.';
+  constructor(private websiteService: WebsiteService,  private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.errorFlag = false;
     this.activatedRoute.params
       .subscribe(
         (params: any) => {
           this.userId = params['uid'];
           this.websites = this.websiteService.findWebsitesByUser(this.userId);
+          this.websitename = '';
+          this.websitedescription = '';
         }
       );
+  }
+  createWebsite() {
+    if (this.websitename === '' || this.websitedescription === '') {
+      this.errorFlag = true;
+    } else {
+      this.aNewWebsite = {_id: Math.random().toString(), name: this.websitename,
+        developerId: this.userId, description: this.websitedescription};
+      this.websiteService.createWebsite(this.userId, this.aNewWebsite);
+      this.router.navigate(['/user/', this.userId, 'website']);
+    }
   }
 }
