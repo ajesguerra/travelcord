f1419a5b0a3addffbad3e183a36d74ebf016f4d0
Arnold Esguerra <ajxesg@gmail.com>
2017-10-15 23:59:20 -0400

    fixed youtube width adjustment display

--- src/app/services/widget.service.client.ts
+++ src/app/services/widget.service.client.ts
@@ -7,9 +7,9 @@ export class WidgetService {
     { '_id': '234', 'widgetType': 'HEADING', 'pageId': '321', 'size': 4, 'text': 'Lorem ipsum'},
     { '_id': '345', 'widgetType': 'IMAGE', 'pageId': '321', 'width': '100%',
       'url': 'http://lorempixel.com/400/200/'},
-    { '_id': '456', 'widgetType': 'HTML', 'pageId': '321', 'text': '<p>Lorem ipsum</p>'},
+    { '_id': '456', 'widgetType': 'HTML', 'pageId': '321', 'text': '<p>Lordsafsdem ipsum</p>'},
     { '_id': '567', 'widgetType': 'HEADING', 'pageId': '321', 'size': 4, 'text': 'Lorem ipsum'},
-    { '_id': '678', 'widgetType': 'YOUTUBE', 'pageId': '321', 'width': '100%',
+    { '_id': '678', 'widgetType': 'YOUTUBE', 'pageId': '321', 'width': '50%',
       'url': 'https://www.youtube.com/embed/VkMU1mKdwPI?ecver=1' },
     { '_id': '789', 'widgetType': 'HTML', 'pageId': '321', 'text': '<p>Lorem ipsum</p>'}
   ];
