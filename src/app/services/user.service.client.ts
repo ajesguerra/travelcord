379857ace332579759ca5008fff8c6a90b3e334f
Arnold Esguerra <ajxesg@gmail.com>
2017-10-25 22:55:54 -0400

    fixed domain url for http requests and ng build for production

--- src/app/services/user.service.client.ts
+++ src/app/services/user.service.client.ts
@@ -1,46 +1,48 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/Rx';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class UserService {
+  baseUrl = environment.baseUrl;
   constructor(private http: Http) {}
 
   findUserByCredentials(username, password) {
-    const url = 'http://localhost:3100/api/user?username=' + username + '&password=' + password;
+    const url = this.baseUrl + '/api/user?username=' + username + '&password=' + password;
     return this.http.get(url).map((response: Response) => {
         return response.json();
     });
   }
   createUser(user: any) {
-    const url = 'http://localhost:3100/api/user/';
+    const url = this.baseUrl + '/api/user/';
     return this.http.post(url, user).map((response: Response) => {
       return response.json();
     });
   }
 
   findUserById(userId: string) {
-    const url = 'http://localhost:3100/api/user/' + userId;
+    const url = this.baseUrl + '/api/user/' + userId;
     return this.http.get(url).map((response: Response) => {
       return response.json();
     });
   }
 
   findUserByUsername(username: string) {
-    const url = 'http://localhost:3100/api/user?username=' + username;
+    const url = this.baseUrl + '/api/user?username=' + username;
     return this.http.get(url).map((response: Response) => {
       return response.json();
     });
   }
 
   updateUser(userId, user) {
-    const url = 'http://localhost:3100/api/user/' + userId;
+    const url = this.baseUrl + '/api/user/' + userId;
     return this.http.put(url, user).map((response: Response) => {
       return response.json();
     });
   }
   deleteUser(user) {
-    const url = 'http://localhost:3100/api/user/' + user._id;
+    const url = this.baseUrl + '/api/user/' + user._id;
     return this.http.delete(url, user).map((response: Response) => {
       return response.json();
     });
--- src/app/services/website.service.client.ts
+++ src/app/services/website.service.client.ts
@@ -1,41 +1,43 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/Rx';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class WebsiteService {
+  baseUrl = environment.baseUrl;
   constructor(private http: Http) {}
   createWebsite(userId, website) {
     website.developerId = userId;
-    const url = 'http://localhost:3100/api/user/' + userId + '/website';
+    const url = this.baseUrl + '/api/user/' + userId + '/website';
     return this.http.post(url, website).map((response: Response) => {
       return response.json();
     });
   }
 
   findWebsitesByUser(userId) {
-    const url = 'http://localhost:3100/api/user/' + userId + '/website';
+    const url = this.baseUrl + '/api/user/' + userId + '/website';
     return this.http.get(url).map((response: Response) => {
       return response.json();
     });
   }
 
   findWebsiteById(websiteId) {
-    const url = 'http://localhost:3100/api/website/' + websiteId;
+    const url = this.baseUrl + '/api/website/' + websiteId;
     return this.http.get(url).map((response: Response) => {
       return response.json();
     });
   }
 
   updateWebsite(websiteId, website)  {
-    const url = 'http://localhost:3100/api/website/' + websiteId;
+    const url = this.baseUrl + '/api/website/' + websiteId;
     return this.http.put(url, website).map((response: Response) => {
       return response.json();
     });
   }
 
   deleteWebsite(websiteId) {
-    const url = 'http://localhost:3100/api/website/' + websiteId;
+    const url = this.baseUrl + '/api/website/' + websiteId;
     return this.http.delete(url, websiteId).map((response: Response) => {
       return response.json();
     });
