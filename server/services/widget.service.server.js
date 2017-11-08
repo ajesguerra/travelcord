052f42ae47094d3b2c02f403d48dfba5f9230b07
Arnold Esguerra <ajxesg@gmail.com>
2017-11-08 15:48:01 -0500

    deleted data from server

--- server/services/widget.service.server.js
+++ server/services/widget.service.server.js
@@ -8,18 +8,6 @@ module.exports = function (app) {
   app.put("/api/widget/:widgetId", updateWidget);
   app.delete("/api/widget/:widgetId", deleteWidget);
 
-  widgets = [
-    { '_id': '123', 'widgetType': 'HEADING', 'pageId': '321', 'size': 2, 'text': 'GIZMODO'},
-    { '_id': '234', 'widgetType': 'HEADING', 'pageId': '321', 'size': 4, 'text': 'Lorem ipsum'},
-    { '_id': '345', 'widgetType': 'IMAGE', 'pageId': '321', 'width': '100%',
-      'url': 'http://lorempixel.com/400/200/'},
-    { '_id': '456', 'widgetType': 'HTML', 'pageId': '321', 'text': '<p>Lordsafsdem ipsum</p>'},
-    { '_id': '567', 'widgetType': 'HEADING', 'pageId': '321', 'size': 4, 'text': 'Lorem ipsum'},
-    { '_id': '678', 'widgetType': 'YOUTUBE', 'pageId': '321', 'width': '50%',
-      'url': 'https://www.youtube.com/embed/VkMU1mKdwPI?ecver=1' },
-    { '_id': '789', 'widgetType': 'HTML', 'pageId': '321', 'text': '<p>Lorem ipsum</p>'}
-  ];
-
   function createWidget(req, res) {
     this.widgets.push(req.body);
     res.json(req.body);
